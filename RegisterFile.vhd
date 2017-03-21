44d84dffeba40ea8801763c3b0542e223aa19563
javs9708 <villegassanchezjhonalexander@gmail.com>
2017-03-21 17:53:05 -0500

    Add files via upload

---
+++ RegisterFile.vhd
@@ -0,0 +1,34 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity RegisterFile is
+    Port ( rs1 : in  STD_LOGIC_VECTOR (4 downto 0);
+           rs2 : in  STD_LOGIC_VECTOR (4 downto 0);
+           rd : in  STD_LOGIC_VECTOR (4 downto 0);
+           rst : in  STD_LOGIC;
+           CRs1 : out  STD_LOGIC_VECTOR (31 downto 0);
+           CRs2 : out  STD_LOGIC_VECTOR (31 downto 0);
+           DWR : in  STD_LOGIC_VECTOR (31 downto 0));
+end RegisterFile;
+
+architecture Behavioral of RegisterFile is
+
+begin
+
+	process(rst,rs1,rs2,rd,DWR)
+	begin
+		
+		if(rst = '1')then
+				crs1 <= (others=>'0');
+				crs2 <= (others=>'0');
+		else
+				crs1 <= rs1;
+				crs2 <= rs2;
+		end if;
+		
+	end process;
+
+end Behavioral;
+
---
+++ tbRegisterFile.vhd
@@ -0,0 +1,65 @@
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+
+ENTITY tbRegisterFile IS
+END tbRegisterFile;
+ 
+ARCHITECTURE behavior OF tbRegisterFile IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT RegisterFile
+    PORT(
+         rs1 : IN  std_logic_vector(4 downto 0);
+         rs2 : IN  std_logic_vector(4 downto 0);
+         rd : IN  std_logic_vector(4 downto 0);
+         rst : IN  std_logic;
+         CRs1 : OUT  std_logic_vector(31 downto 0);
+         CRs2 : OUT  std_logic_vector(31 downto 0);
+         DWR : IN  std_logic_vector(31 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal rs1 : std_logic_vector(4 downto 0) := (others => '0');
+   signal rs2 : std_logic_vector(4 downto 0) := (others => '0');
+   signal rd : std_logic_vector(4 downto 0) := (others => '0');
+   signal rst : std_logic := '0';
+   signal DWR : std_logic_vector(31 downto 0) := (others => '0');
+
+ 	--Outputs
+   signal CRs1 : std_logic_vector(31 downto 0);
+   signal CRs2 : std_logic_vector(31 downto 0);
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
+ 
+
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: RegisterFile PORT MAP (
+          rs1 => rs1,
+          rs2 => rs2,
+          rd => rd,
+          rst => rst,
+          CRs1 => CRs1,
+          CRs2 => CRs2,
+          DWR => DWR
+        );
+
+   stim_proc: process
+   begin		
+    
+		rs1<=x"00001";
+		wait for 20 ns;
+		rs2<=x"00002";
+		wait for 20 ns;
+
+      wait;
+   end process;
+
+END;
